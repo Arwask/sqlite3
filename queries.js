101b2e2d051667f3485cf5475b74ea5c28ca3dc5
Arwa <arwa.it15@gmail.com>
2017-08-29 14:50:13 -0500

    implemented queries but its asynchronous

--- queries.js
+++ queries.js
@@ -7,13 +7,18 @@ const db = new sqlite3.Database('company.sqlite', (err) => { //checks if it exis
     console.log('Connected');
 });
 
-db.run('DROP TABLE IF EXISTS employees');
+db.run('DROP TABLE IF EXISTS employees', () => {
+    createEmployeesTable();
+});
 
 const createEmployeesTable = () => {
-    db.run('CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT, salary INT, department TEXT)');
+    db.run('CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT, salary INT, department TEXT)', () => {
+        console.log("created");
+        populateEmployees();
+        getFilteredData();
+    });
 };
 
-createEmployeesTable();
 
 // db.run('INSERT INTO employees VALUES (1, "Fred", "Jones", 45000, "Sales")');
 // db.run('INSERT INTO employees VALUES (2, "Fredrica", "Jonathon", 75000, "CEO")');
@@ -21,39 +26,54 @@ createEmployeesTable();
 const populateEmployees = () => {
     const { list } = require('./employees.json');
 
-    list.forEach( (employee) => {
-        console.log(employee);
-        db.run(`INSERT INTO employees VALUES(
-            ${employee.id},
-            "${employee.firstName}",
-            "${employee.lastName}",
-            ${employee.salary},
-            "${employee.department}")`)
-    })
+        list.forEach( (employee) => {
+            db.run(`INSERT INTO employees VALUES(
+                ${employee.id},
+                "${employee.firstName}",
+                "${employee.lastName}",
+                ${employee.salary},
+                "${employee.dept}")`, () => {
+                    console.log("Done");
+                    getFilteredData();
+                })
+        });
+    console.log("get filtered data");
 };
 
-populateEmployees();
 
-// db.get('SELECT * FROM employees')
+// db.get('SELECT * FROM employees') <---- db.get gets only one data even though you asked for all
+// const getFilteredData = () => {
 
-// db.all('Select * from employees', (err, allRows) => {
-//     // console.log("ALL Data", allRows);
-
-//     if(err) return console.log('err', err.toString());
-
-//     //sort alphabetically by first name
-//     // make array of emps whose salary > 50
-//     allRows.sort( (a,b) => a.first.localeCompare(b.first) ) //localeCompare (google it) : sorts string and returns negative, positive or 0
-//     .filter( (each) => each.salary > 50000)
-//     .map( (emp) => {
-//         `${emp.first} ${emp.last}'s salary: ${emp.salary}`
-//         console.log(`${emp}`)
+//     db.all('Select * from employees', (err, allRows) => {
+//         // console.log("ALL Data", allRows);
+    
+//         if(err) 
+//             return console.log('err', err.toString());
+    
+//         //sort alphabetically by first name
+//         // make array of emps whose salary > 50
+//         allRows.sort( (a,b) => a.first.localeCompare(b.first) ) //localeCompare (google it) : sorts string and returns negative, positive or 0
+//         .filter( (emp) => emp.salary > 5000)
+//         .map( (emp) => {
+//             `${emp.first} ${emp.last}'s salary: ${emp.salary}`
+//             console.log(emp);
+//         })
 //     })
-// })
- 
+// }
 
-// db.each(`select * from employees`, (err, {id, first, last, deaprtment, salary}) => {
-//     if(err) 
-//         console.log(err.toString());
-//     console.log(`${id} ${frst} ${last}`)
-// })
+const getFilteredData = () => {
+    console.log("where???");
+    db.each(`select * from employees`, (err, {id, first, last, deaprtment, salary}) => {
+        if(err) 
+            console.log(err.toString());
+        console.log(`${id} ${first} ${last}`)
+    })
+
+
+    db.all(`select * from employees`, (err, data) => {
+        console.log("data??", data)
+                if(err)
+                    console.log("Errrr:", err.toString());
+                return data;
+            });
+}
